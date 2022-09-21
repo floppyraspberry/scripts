bc2f3d824c38ed630aba4456399b39ebc36e570c
Remco Ridder <remco.ridder@knab.nl>
2022-09-21 16:27:51 +0200

    Create Profile.ps1
    added PS Profile file

---
+++ Profile.ps1
@@ -0,0 +1,111 @@
+## Uncomment return to exit the script if needed
+# return
+
+## TODO: fix $defaultLocation, fix if/else logic
+## RUN Command: . $Profile
+
+# Style default PowerShell Console
+$shell = $Host.UI.RawUI
+$shell.WindowTitle= "PS"
+$shell.BackgroundColor = "Black"
+$shell.ForegroundColor = "White"
+$host.ui.RawUI.ForegroundColor = $t
+
+# Set variables
+$dateTime = Get-Date
+[Environment]::SetEnvironmentVariable('$date', $dateTime.Date)
+[Environment]::SetEnvironmentVariable('$hour', $time.Hour)
+$defaultLocation = [Environment]::GetEnvironmentVariable('defaultLocation')
+
+# Load custom theme for Windows Terminal
+Import-Module posh-git
+Import-Module oh-my-posh
+
+# Set Default location
+Set-Location $defaultLocation
+oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+
+## Not sure what was intended here:
+# Lazy way to use scripts as module:
+Set-Alias ConnectTo-SharePointAdmin ConnectTo-SharePointAdmin.ps1
+Set-Alias ConnectTo-EXO ConnectTo-EXO.ps1
+Set-Alias Get-MFAStatus MFAStatus.ps1
+Set-Alias Get-MailboxSizeReport MailboxSizeReport.ps1
+Set-Alias Get-OneDriveSizeReport OneDriveSizeReport.ps1
+
+# Print to screen at startup
+if (($hour -gt "6") -and ($time -lt "12"))
+{ Write-Host("Good morning $env:UserName `n") }
+
+if (($time -gt "12") -and ($time -lt "18"))
+{ Write-Host("Good morning $env:UserName `n") }
+
+if (($time -gt "18") -and ($time -lt "23"))
+{ Write-Host("Good morning $env:UserName `n") }
+
+if (($time -gt "23") -and ($time -lt "06"))
+{ Write-Host("Good night $env:UserName `n") }
+
+if (($time -gt "8") -and ($time -lt "10"))
+{
+    # update choco packages 
+    
+    Write-Host "Updating some systems...."
+    gsudo choco upgrade all -y
+
+    # $diff = Compare-Object (-ReferenceObject [Environment]::GetEnvironmentVariable('time') -DifferenceObject $time )
+    # $oldTime = [Environment]::GetEnvironmentVariable('time')
+    # $((Get-Date).AddMinutes($oldTime))
+    # if ($diff -gt 16)
+    {
+        # Write-Host 
+        # Write-Host $oldTime
+        # Write-Host "Updating some systems...."
+        # gsudo choco upgrade all -y
+    }
+    # if ($diff -lt 24)
+    # {
+    #     Write-Host
+    #     Write-Host "Nothing updated...."
+    #     Write-Host $oldTime
+    # }
+}
+
+# notes and reminders
+Write-Host ''
+$t = $host.ui.RawUI.ForegroundColor
+$host.ui.RawUI.ForegroundColor = “DarkGreen”
+Write-Host ''
+
+Write-Host 'Please remember: '
+
+Write-Host 'Environment variables:' 'refreshenv', 'dir env:'
+Write-Host ''
+Write-Host 'Set default location from environment vars:'
+Write-Host $defaultLocation
+Write-Host ''
+Write-Host 'Searching: '
+Write-Host 'Get-Content file -wait'
+Write-Host 'Get-ChildItem -Recurse | Where {$_.Name -match 'specflow'} | Select Fullname' '==> looks for file name'
+Write-Host 'Select-String -Path $defaultLocation -Pattern 'Safari' -quiet' '==> looks for file name'
+Write-Host 'Get-ChildItem -Recurse | Select-String "dummy" -List | Select Path', '==> looks for string in files'
+Write-Host ''
+Write-Host 'Create/delete stuff: '
+Write-Host 'echo $null >> filename'
+Write-Host ''
+Write-Host 'Temp cleanup:' 
+Write-Host 'Remove-Item C:\Temp -Verbose'  
+Write-Host ''
+Write-Host 'Delete folder content:', 'Get-ChildItem -Path C:\Temp | Remove-Item -Recurse -Force'
+Write-Host ''
+Write-Host 'Other: '
+Write-Host 'Administrator terminal:' 'sudo wt'
+Write-Host 'count string characters:' '$text | Measure-Object -Character'
+Write-Host 'Compare diff:' 'Compare-Object -ReferenceObject (Get-Content -Path C:\Temp\Testfile1.txt) -DifferenceObject (Get-Content -Path C:\Temp\Testfile2.txt)'
+Write-Host ''
+Write-Host 'Your Powershell version of today is: '($PsVersionTable.PSVersion )''
+
+# Estimated battery runtime
+Write-Host ''
+$MinutesRunTime = (Get-CimInstance win32_battery).EstimatedRunTime 
+"The laptop will shut down at ~ $((Get-Date).AddMinutes($MinutesRunTime))"
